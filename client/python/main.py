413669264ef7db095c714b24c60aa1e15ca3696b
Pultak <pultak5@gmail.com>
2022-11-25 20:50:45 +0100

    Definition of API

--- client/python/main.py
+++ client/python/main.py
@@ -1,8 +1,70 @@
+import os
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import logging
+import socket
+import time
+
+from flask import Flask, request, jsonify
+
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
+                    level='INFO')
+app = Flask(__name__)
+# hack to let the vagrant setup eth1 interface for http server
+time.sleep(2)
+
+root_flag = False
+
+
+@app.put("/store/")
+def store_value():
+    # todo
+    new_value = request.get_data()
+
+    return "Ok"
+
+
+@app.get('/receive/<>')
+def return_desired_value():
+    # todo
+    new_value = request.get_data()
+
+    return "NotFound"
+
+
+@app.delete('/remove/')
+def remove_value():
+    # todo
+    new_value = request.get_data()
+
+    return "NotFound"
+
+
+@app.get('/update/')
+def send_cache_update():
+    # todo send update on value change
+
+    return ""
+
+
+def register_to_zookeeper():
+
+    # todo register
+    return
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    global root_flag
+    parent_address = os.environ['PARENT_NODE']
+    node_address = os.environ['NODE_ADDRESS']
+
+    logging.info('Welcome to distributed binary tree cache client!')
+
+    if parent_address == "ROOT":
+        # this client is root
+        root_flag = True
+        logging.info('Im ROOT of the tree (' + node_address + ')!')
+    else:
+        logging.info('Im LEAF of the tree (' + node_address + ')! My parent is ' + parent_address)
+
+    # todo get/check correct address
+    app.run(host=str(node_address))
